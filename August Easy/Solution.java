d9fef20273560c29ad882a2e72364014ce61f2fe
Harshit <love.coolsachin@gmail.com>
2017-08-02 00:55:57 +0530

    Update Solution.java

--- August Easy/Solution.java
+++ August Easy/Solution.java
@@ -1,19 +1,9 @@
-/* IMPORTANT: Multiple classes and nested static classes are supported */
-
-/*
- * uncomment this if you want to read input.
-//imports for BufferedReader
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-*/
-import java.util.ArrayList;
 import java.util.*;
-
 
 class TestClass 
 {
     public static void main(String args[] ) throws Exception {
-        long max=0,ekaurvar=0,dusravar;
+        int max=0,temp=0,temp1;
         int i;
         long mod=1000000007,product=1;
         Scanner s = new Scanner(System.in);
@@ -23,29 +13,22 @@ class TestClass
         while(N-->0)
         {
             int t=s.nextInt();
-            ekaurvar=t;
+            temp=t;
             if(t>max)
-              {
-               
-                
-                dusravar=max;
-              for(i=(int)dusravar+1;i<t+1;i++)
-              {
-                  product*=list.get(i-1);
-                  product=product%mod;
-                  list.add(product+1);
-              }
-              
-              max=ekaurvar;
-              }
-              for(i=0;i<(int)ekaurvar;i++)
-              System.out.print(list.get(i)+" ");
-              System.out.println();
-              
+            {
+                temp1=max;
+                for(i=temp1+1;i<t+1;i++)
+                {
+                     product*=list.get(i-1);
+                     product=product%mod;
+                     list.add(product+1);
+                }
+                max=temp;
             }
-            
-        }
-
+            for(i=0;i<temp;i++)
+            System.out.print(list.get(i)+" ");
+            System.out.println();
+              
+        }        
     }
-
-
+}
